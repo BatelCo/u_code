db097f162b9b1b5be932595c2ce05a16c6e1c99e
batelco <batel.cohen100@gmail.com>
2022-08-22 07:56:52 +0300

    tweeter dara module done

--- week2/closures-modules/main.js
+++ week2/closures-modules/main.js
@@ -206,6 +206,24 @@ songsManager.addSong("ain't me", "https://www.youtube.com/watch?v=D5drYkLiLI8")
 
 songsManager.getSong("sax") // should print https://www.youtube.com/watch?v=3JZ4pnNtyxQ
 
-// hi
+// Comprehension Check
+let _memories = ["that time at band camp", "yesterday's breakfast"]
+    
+const Brain = function(){
+    const dayDream = () => _memories
+    const experience = memory => _memories.push(memory)
+    const forget = memory => _memories.splice(_memories.indexOf(memory), 1)
+    const spazz = () => console.log("bleurg")
+
+    return{
+        reminisce: dayDream,
+        live: experience,
+        blank: forget,
+        freakOut: spazz
+    }
+}
+
+const brain = Brain()
+brain.freakOut()
 
 
---
+++ week2/tweeter-data-module/main.js
@@ -0,0 +1,71 @@
+console.log("Hello, world!")
+
+const LogicTweeter = function () {
+    const _posts = []
+    let _nextIdComment = 1;
+    let _nextIdPost = 1;
+    const idPostMachanism = () => `p${_nextIdPost++}`
+    const idCommentMachanism = () => `c${_nextIdComment++}`
+    const getPosts = () => _posts
+
+    const addPost = (str) => {
+        _posts.push({ text: str, id: idPostMachanism(), comments: [] })
+    }
+
+    const removePost = pid => {
+        for (let index in _posts) {
+            if (_posts[index].id === pid) {
+                _posts.splice(index, 1)
+            }
+        }
+    }
+
+    const addComment = (postId, textComment) => {
+        for (let index in _posts) {
+            if (_posts[index].id === postId) {
+                _posts[index].comments.push({ id: idCommentMachanism(), text: textComment })
+            }
+        }
+    }
+
+    const removeComment = (pid, cid) => {
+        for (let index in _posts) {
+            if (_posts[index].id === pid) {
+                for (let indexComm in _posts[index].comments) {
+                    if (_posts[index].comments[indexComm].id === cid) {
+                        _posts[index].comments.splice(indexComm, 1)
+                    }
+                }
+            }
+        }
+    }
+
+    return {
+
+        getPosts: getPosts,
+        addPost: addPost,
+        removePost: removePost,
+        addComment: addComment,
+        removeComment: removeComment
+    }
+}
+
+
+const tweeter = LogicTweeter();
+tweeter.addPost("This is post!");
+tweeter.addPost("2");
+tweeter.addPost("3");
+tweeter.addPost("4");
+tweeter.addPost("5");
+console.log(tweeter.getPosts());
+tweeter.removePost("p2");
+tweeter.removePost("p3");
+console.log(tweeter.getPosts());
+tweeter.addComment("p1", "aiiiii");
+tweeter.addComment("p1", "biiiii");
+tweeter.addComment("p4", "diiiii");
+tweeter.addComment("p5", "eiiiii");
+console.log(tweeter.getPosts());
+tweeter.removeComment("p1", "c2");
+tweeter.removeComment("p4", "c3");
+console.log(tweeter.getPosts());
