af7337b91634def79056d7a70a568ce1f34c956d
batelco <batel.cohen100@gmail.com>
2022-09-02 15:42:21 +0300

    extention of unit tests with jest exercises

--- week3/unit-tests-with-jest/exercises.test.js
+++ week3/unit-tests-with-jest/exercises.test.js
@@ -1,7 +1,7 @@
 const EX = require('./exercises')
 const exercises = new EX()
 
-//ex1
+// ex1
 test("isEven should return true if n is evens", function () {
 
     expect(exercises.isEven(4)).toBeTruthy()
@@ -12,7 +12,7 @@ test("isEven should return true if n is evens", function () {
 
 })
 
-//ex2
+// ex2
 test("removeAtLeastOne should remove at least one element from the array `arr`", function () {
     const arr = [1, 2, 3, 4, 5]
     let length = arr.length
@@ -21,7 +21,7 @@ test("removeAtLeastOne should remove at least one element from the array `arr`",
 
 })
 
-//ex3
+// ex3
 test("simplify should return a clean string without these symbols:! # . , ", function () {
     const str1 = "nir#"
     const str2 = "nir"
@@ -30,16 +30,23 @@ test("simplify should return a clean string without these symbols:! # . , ", fun
 
 })
 
-//ex4
+// ex4
 test("validate should recive arr and verify that there is at least one boolean in the array", function () {
     const errorArr = [1, 2, 3]
     const arrValid = [true, false, true]
     const arrInvalid = [false, false, true]
-
     const error = { error: "Need at least one boolean" }
-
     expect(exercises.validate(errorArr)).toEqual(error)
     expect(exercises.validate(arrValid)).toBeTruthy()
     expect(exercises.validate(arrInvalid)).toBeFalsy()
 
+})
+
+// extention
+test("checks whether push has been used in add method", () => {
+    const spyOnAdd = jest.spyOn(exercises, "add") //creating a spying object on 'add' method, not neccesery
+    const spyOnPush = jest.spyOn(Array.prototype, "push") //creating the spying object for push, that actually belong to Array.prototype.
+    const isAdding = exercises.add(1, 2) //invocing a mock call to the add method, so we can spy on its behaviour
+    expect(spyOnAdd).toHaveBeenCalled() //just to verify we are calling add method, not neccesery
+    expect(spyOnPush).toHaveBeenCalled() //tracking push is called at least 1 time
 })
