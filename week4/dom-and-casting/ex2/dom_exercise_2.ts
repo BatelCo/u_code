008f10d5a8b33a3b7410d91726ac89f2b067f2af
batelco <batel.cohen100@gmail.com>
2022-09-05 16:33:29 +0300

    changes

---
+++ week4/dom-and-casting/ex2/dom_exercise_2.ts
@@ -0,0 +1,7 @@
+function initPage() {
+    const domElement = document.querySelector('.foo') as HTMLInputElement;
+    domElement.addEventListener('input', function (event) {
+        const e = event.target as HTMLInputElement;
+        console.log(e.value);
+    });
+}
