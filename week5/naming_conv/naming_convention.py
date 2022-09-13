3b5b84b001500b28cc214352a3bee29b969eb491
batelco <batel.cohen100@gmail.com>
2022-09-13 08:59:55 +0300

    update folder naming conv

---
+++ week5/naming_conv/naming_convention.py
@@ -0,0 +1,24 @@
+def get_store_name():
+    pass
+
+
+def add_student():
+    pass
+
+
+def restore_product():
+    pass
+
+
+# class WordCounter():
+# class Bank():
+
+# moudles
+# products.py
+# math_utils.py
+
+# not allowd
+# math.utils.py # dot
+# math-utils.py # dash
+# Users.py # capital letter
+# 2users.py # start with a digit
