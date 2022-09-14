0bed6c3e2ae341648ccceddbeb5d78837df10810
batelco <batel.cohen100@gmail.com>
2022-09-14 08:59:26 +0300

    oop intro

---
+++ week5/oop-intro/person.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print("Hi there, I am " + self.name)
+
+
+p1 = Person("Jezrien", 102)
+p2 = Person("Tal", 96)
+p1.introduce()  # outputs: Hi there, I am Jezerien
+p2.introduce()  # outputs: Hi there, I am Tal
