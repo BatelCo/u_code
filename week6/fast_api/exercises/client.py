dfef3d5f7382dc7e2ba121bd0b3b2994ff72c9ea
batelco <batel.cohen100@gmail.com>
2022-09-19 12:31:55 +0300

    fast api

--- week6/fast_api/exercises/client.py
+++ week6/fast_api/exercises/client.py
@@ -2,38 +2,45 @@
 
 import requests
 
-# ex3
+
+class Client:
+    def __init__(self, money):
+        self.money = money
+
+    def get_furniture_price(self, furniture_name):
+        res = requests.get(
+            'http://localhost:3000/store/{}'.format(furniture_name))
+        return (res.json())
+
+    def buy_furniture(self, furniture_name):
+        res = requests.get(
+            'http://localhost:3000/buy/{}'.format(furniture_name))
+        name = res.json()["name"]
+        price = res.json()["price"]
+        inventory = res.json()["inventory"]
+        print(
+            f"Congratulations, you've just bought {name} for {price}. There are {inventory} left now in the store.")
 
 
-def get_furniture_price(furniture_name):
-    res = requests.get(
-        'http://localhost:3000/store/{}'.format(furniture_name))
-    return (res.json())
+client = Client(200)
 
 
-print('Enter furniture name:')
-furniture_name = input()
-print(get_furniture_price(furniture_name))
-
-# ex5 +  Extension 1
-money = 200
+def get_item_by_user():  # ex3
+    print('Enter furniture name:')
+    furniture_name = input()
+    print(client.get_furniture_price(furniture_name))
 
 
-def buy_furniture(furniture_name):
-    res = requests.get(
-        'http://localhost:3000/buy/{}'.format(furniture_name))
-    name = res.json()["name"]
-    price = res.json()["price"]
-    inventory = res.json()["inventory"]
-    print(
-        f"Congratulations, you've just bought {name} for {price}. There are {inventory} left now in the store.")
+def user_buy():  # ex5 +  Extension 1
+    print('What furniture do you want to buy?')
+    furniture_name = input()
+    furniture_price = client.get_furniture_price(furniture_name)["price"]
+    if (client.money >= furniture_price):
+        client.buy_furniture(furniture_name)
+        client.money = client.money - furniture_price
+    else:
+        print("you should get a job")
 
 
-print('What furniture do you want to buy?')
-furniture_name = input()
-furniture_price = get_furniture_price(furniture_name)["price"]
-if (money >= furniture_price):
-    buy_furniture(furniture_name)
-    money = money - furniture_price
-else:
-    print("you should get a job")
+get_item_by_user()
+user_buy()
--- week6/fast_api/exercises/exercises_server.py
+++ week6/fast_api/exercises/exercises_server.py
@@ -1,7 +1,5 @@
 #  exercises_server.py (the server)
 
-import requests
-from fastapi.responses import FileResponse
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -37,32 +35,26 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @ app.get("/store/{name}")
 async def get_item_price(name):
-    # for item in store:
-    #     if (item["name"] == name):
-    #         print(item)
-    #         price = item["price"]
-    #         return ({"price": price})
-    # return {"price": None}
     item_name_price = [{"price": item["price"]}
                        for item in store if item["name"] == name]
     return item_name_price[0] if (len(item_name_price) > 0) else {"price": None}
 
 
 # ex4
-@ app.get("/buy/{name}")
-async def reduce_inventory(name):
+@ app.get("/buy/{furniture_name}")
+async def reduce_inventory(furniture_name):
     for item in store:
-        if item["name"] == name:
+        if item["name"] == furniture_name:
             item["inventory"] = item["inventory"] - 1
             return item
 
 
-# http://localhost:3000/sale/?is_admin=true
+# http://localhost:3000/sale?is_admin=true
 # ex6
-@ app.get("/sale/")
+@ app.get("/sale")
 async def sale(is_admin):
-    if (is_admin == "true"):
+    if (bool(is_admin)):
         for item in store:
-            if item["inventory"] > 10:
-                item["price"] = item["price"]*0.5
+            item["price"] = item["price"] * \
+                0.5 if item["inventory"] > 10 else item["price"]
     return store
