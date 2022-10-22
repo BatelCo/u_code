14940f08aa67f1adf1bbf5babc8737b094b978bc
batelco <batel.cohen100@gmail.com>
2022-10-22 22:42:51 +0300

    pymysql

---
+++ week9/sql/pymysql/testing.py
@@ -0,0 +1,31 @@
+import pymysql 
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="jobs",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor
+)
+if connection.open:
+    print("the connection is opened")
+
+
+# try:
+#     with connection.cursor() as cursor:
+#         query = 'INSERT into company (name,industry, employees) values ("Wix", "hi tech", 4000)'
+#         cursor.execute(query)
+#         connection.commit()
+# except:
+#     print("Error")
+
+try:
+    with connection.cursor() as cursor:
+        query = "SELECT name FROM company"
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for company in result:
+            print(company["name"])
+except:
+    print("DB Error")
+
